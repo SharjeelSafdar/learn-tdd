c7c949198f1b0f5e71f0ea4913615468f44df987
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-17 10:30:52 +0500

    Initial commit + Travice CI YAML file

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello, World', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello, World/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Hello, PIAIC', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello, PIAIC/i);
   expect(linkElement).toBeInTheDocument();
 });
