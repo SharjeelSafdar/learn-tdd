73969ddcabf8cae865551328d1dedadb3436f625
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-17 14:42:23 +0500

    Another commit

--- src/About.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import About from './About';
-
-test('should render "About Page"', () => {
-  const { getByText } = render(<About />);
-  const linkElement = getByText(/About Page/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import About from './About';
 
 function App() {
   return (
     <div className="App">
       <p>Hello, World!</p>
       <p>Hello, PIAIC!</p>
+      <About />
     </div>
   );
 }
